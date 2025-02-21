d7ed27a9184d305b7d25abd76cb05bb783dc01d8
jakelockitch <j.lockitch@student.maastrichtuniversity.nl>
2025-02-21 10:28:30 +0100

    added super basic encryption. TODO: make it better

--- app.py
+++ app.py
@@ -13,14 +13,16 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
 
+
 def is_strong_password(password):
-    return(
+    return (
         len(password) >= 8
         and any(c.isdigit() for c in password)
         and any(c.isupper() for c in password)
         and any(c.islower() for c in password)
         and any(c in "!@#$%^&*()-_=+" for c in password)
     )
+
 
 # Home page: shows different content based on whether the user is logged in
 @app.route("/")
@@ -46,7 +48,9 @@ def login():
         password = request.form["password"]
 
         # Look up the user in the database
-        user = User.query.filter_by(username=username, password=password).first()
+        user = User.query.filter_by(
+            username=username, password=decrypt_password(password)
+        ).first()
         if user:
             session["username"] = user.username
             return redirect(url_for("home"))
@@ -78,15 +82,15 @@ def signup():
         existing_user = User.query.filter_by(username=username).first()
         if existing_user:
             return "Username already exists.<br><a href='/signup'>Try again</a>"
-        
+
         if not is_strong_password(password):
             return (
                 "Password must be at least 8 characters long, contain an uppercase letter, "
                 "a lowercase letter, a number, and a special character.<br><a href='/signup'>Try again</a>"
-                )
+            )
         else:
             # Create a new user record and save it to the database
-            new_user = User(username=username, password=password)
+            new_user = User(username=username, password=encrypt_password(password))
             db.session.add(new_user)
             db.session.commit()
             return "User created!<br><a href='/login'>Login now</a>"
@@ -128,6 +132,21 @@ def logout():
     return redirect(url_for("home"))
 
 
+def encrypt_password(pword):
+    result = []
+    for c in pword:
+        # Shift character by 2 positions
+        result.append(chr(ord(c) + 3))
+    return "".join(result)
+
+def decrypt_password(pword):
+    result = []
+    for c in pword:
+        # Shift character back by 2 positions
+        result.append(chr(ord(c) - 3))
+    return "".join(result)
+
+
 # Run the Flask app
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=5002)
+    app.run(debug=True, host="0.0.0.0", port=5002)
